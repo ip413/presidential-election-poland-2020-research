13a26a86d01ef4f80c4eaa65095cf1b6a32005b2
github:ip413 <ip413as@gmail.com>
2020-07-17 20:04:12 +0200

    normal dist model

--- r/04-histogram-comparison.r
+++ r/04-histogram-comparison.r
@@ -96,21 +96,55 @@ X11()
 
 #---------------------------------------- round 2 urban
 
-df2urban <- df2[df2[, "typ_obszaru"]=="miasto", ]
+# df2urban <- df2[df2[, "typ_obszaru"]=="miasto", ]
 
-png("r-results/04-histogram-comparison/04-histogram-comparison-duda-urban-curve-round-2.png", width = 600, height = 480)
-x <- df2urban$result_duda
-hist(x, breaks = 200, xlab = "result [%]", ylim = range(0, 0.041), main = "duda, urban areas - round 2", freq=F)
-s = sd(x)
-m = mean(x)
-lines(seq(100), dnorm(seq(100), mean(x), sd(x)), ylim = range(0, 0.041), col="red")
+# png("r-results/04-histogram-comparison/04-histogram-comparison-duda-urban-curve-round-2.png", width = 600, height = 480)
+# x <- df2urban$result_duda
+# hist(x, breaks = 200, xlab = "result [%]", ylim = range(0, 0.041), main = "duda, urban areas - round 2", freq=F)
+# s = sd(x)
+# m = mean(x)
+# lines(seq(100), dnorm(seq(100), mean(x), sd(x)), ylim = range(0, 0.041), col="red")
 
-png("r-results/04-histogram-comparison/04-histogram-comparison-trzaskowski-urban-curve-round-2.png", width = 600, height = 480)
-x <- df2urban$result_trzaskowski
-hist(x, breaks = 200, xlab = "result [%]", ylim = range(0, 0.041), main = "trzaskowski, urban areas - round 2", freq=F)
-s = sd(x)
-m = mean(x)
-lines(seq(100), dnorm(seq(100), mean(x), sd(x)), ylim = range(0, 0.041), col="red")
+# png("r-results/04-histogram-comparison/04-histogram-comparison-trzaskowski-urban-curve-round-2.png", width = 600, height = 480)
+# x <- df2urban$result_trzaskowski
+# hist(x, breaks = 200, xlab = "result [%]", ylim = range(0, 0.041), main = "trzaskowski, urban areas - round 2", freq=F)
+# s = sd(x)
+# m = mean(x)
+# lines(seq(100), dnorm(seq(100), mean(x), sd(x)), ylim = range(0, 0.041), col="red")
+
+#---------------------------------------- round 2 normal distribution
+
+# Function change values below min and above max to min and max
+limit_vector_values <- function(v, min, max) {
+    index<-0
+    for(i in v) {
+        index<-index+1;
+        # print(i)
+        if(i < min) {
+            v[[index]] = min
+        }
+
+        if(i > max) {
+            v[[index]] = max
+        }
+    }
+    return(v)
+}
+set.seed(15-09-2020)
+df2rnormDuda = rnorm(27000, mean=mean(df2$result_duda), sd=sd(df2$result_duda))
+df2rnormTrzaskowski = rnorm(27000, mean=mean(df2$result_trzaskowski), sd=sd(df2$result_trzaskowski))
+
+png("r-results/04-histogram-comparison/04-histogram-comparison-duda-normal-dist-model.png", width = 600, height = 480)
+df2rnormDudaLimited <- limit_vector_values(df2rnormDuda, 0, 100)
+hist(df2rnormDudaLimited, breaks=100, main="duda, round 2, normal distribution model", freq=F)
+lines(seq(100), dnorm(seq(100), mean(df2rnormDudaLimited), sd(df2rnormDudaLimited)), ylim = range(0, 0.031), col="red")
+
+png("r-results/04-histogram-comparison/04-histogram-comparison-trzaskowski-normal-dist-model.png", width = 600, height = 480)
+df2rnormTrzaskowskiLimited <- limit_vector_values(df2rnormTrzaskowski, 0, 100)
+hist(df2rnormTrzaskowskiLimited, breaks=100, main="trzaskowski, round 2, normal distribution model", freq=F)
+lines(seq(100), dnorm(seq(100), mean(df2rnormTrzaskowskiLimited), sd(df2rnormTrzaskowskiLimited)), ylim = range(0, 0.031), col="red")
+
+
 
 
 prompt  <- "hit spacebar to close plots"
